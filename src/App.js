aeee80745d05292ecb9c2103cea9325127c77418
navdeep <navdeepdahiya753@gmail.com>
2021-08-28 13:30:18 +0530

    update

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <h1>I am learning git</h1>
+        <h1>I am learning githh</h1>
         <a
           className="App-link"
           href="https://reactjs.org"
